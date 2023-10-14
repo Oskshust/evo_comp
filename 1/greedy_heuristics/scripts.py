3737958c85d91d3874a2d39822abd4cf9b6d04d6
AlicjaKasicka <96540319+AlicjaKasicka@users.noreply.github.com>
2023-10-14 18:42:07 +0200

    Add nn methods

--- 1/greedy_heuristics/scripts.py
+++ 1/greedy_heuristics/scripts.py
@@ -74,3 +74,62 @@ def run_random_exp(path: str):
     print("Mean cost after 200 solutions: " + str(avg_cost))
     
     show_solution(path, best_sol)
+
+
+def get_dist_nn_matrix(path: str):
+    with open(path, 'r') as f:
+        reader = csv.reader(f, delimiter=';')
+        data = list(reader)
+
+    data = np.array(data).astype(int)
+
+    coords = data[:, :2]
+
+    distances = np.round(np.sqrt(np.sum((coords[:, None, :] - coords[None, :, :]) ** 2, axis=-1))).astype(float)
+    distances[distances == 0] = np.inf
+
+    return distances
+
+
+def nn_solution(matrix, matrix_nn):
+    nn_matrix = matrix_nn.copy()
+    n = math.ceil(matrix.shape[0] / 2)
+
+    # choosing first solution randomly
+    v1 = np.random.choice(matrix.shape[0])
+    v = v1
+    sol = [v1]
+
+    # choosing the next n-1 solutions
+    for i in range(n-1):
+        nn = np.argmin(nn_matrix[v])
+        nn_matrix[v, :] = np.inf
+        nn_matrix[:, v] = np.inf
+        v = nn
+        sol.append(v)
+    
+    sol.append(v1)
+    cost = sum(matrix[sol[i-1], sol[i]] for i in range(n))
+    
+    return sol, cost
+
+
+def run_nn_exp(path: str):
+    matrix = get_dist_matrix(path)
+    nn_matrix = get_dist_nn_matrix(path)
+    
+    solutions = []
+
+    for _ in range(200):
+        solutions.append(nn_solution(matrix, nn_matrix))
+
+    costs = np.array([cost for sol, cost in solutions])
+    best_sol, best_cost = min(solutions, key=lambda x: x[1])
+    worst_cost = np.max(costs)
+    avg_cost = np.mean(costs)
+    
+    print("Best cost: " + str(best_cost))
+    print("Worst cost: " + str(worst_cost))
+    print("Mean cost after 200 solutions: " + str(avg_cost))
+    
+    show_solution(path, best_sol)
