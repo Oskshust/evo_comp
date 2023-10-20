3222d87d655f0d772fb8eb4a656109c2b6c8b4c2
Oskar Szudzik <oszudzik@gmail.com>
2023-10-20 16:40:19 +0200

    prepare structure, report and scripts for the 2nd assignment

---
+++ 2/scripts.py
@@ -0,0 +1,92 @@
+import csv
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def get_coords_n_costs(path: str):
+    with open(path, 'r') as f:
+        reader = csv.reader(f, delimiter=';')
+        data = list(reader)
+
+    data = np.array(data).astype(int)
+
+    coords = data[:, :2]
+    costs = data[:, 2]
+
+    return coords, costs
+
+def get_dist_matrix(path: str):
+    coords, costs = get_coords_n_costs(path)
+
+    distances = np.round(np.sqrt(np.sum((coords[:, None, :] - coords[None, :, :]) ** 2, axis=-1))).astype(float)
+    distances[distances == 0] = np.inf
+
+    return distances + costs
+
+def calculate_cost(solution, matrix):
+    return sum(matrix[solution[i-1], solution[i]] for i in range(len(solution)))
+
+def show_solution(path, solution, title):
+    coords, node_costs = get_coords_n_costs(path)
+    plt.figure(figsize=(10, 10))
+    plt.scatter(coords[:, 0], coords[:, 1], c=node_costs, cmap='plasma')
+
+    best_tour_coords = np.append(solution, solution[0])
+    plt.plot(coords[best_tour_coords, 0], coords[best_tour_coords, 1], 'r-')
+
+    plt.colorbar(label='Cost')
+    plt.title(title)
+    plt.show()
+
+def regret2(matrix_src, start_v):
+    matrix = matrix_src.copy()
+    n = math.ceil(matrix.shape[0] / 2)
+    
+    next_v = np.argmin(matrix[start_v])
+
+    cycle = [start_v, next_v]
+    cost = calculate_cost(cycle, matrix_src)
+    matrix[:, [start_v, next_v]] = np.inf
+
+    for _ in range(n - 2):
+        best_new_cost = np.inf
+        best_new_cycle = cycle
+        chosen_v = 0
+
+        for i, v in enumerate(cycle):
+            closest_v = np.argmin(matrix[v])
+            new_cycle = cycle[:i] + [closest_v] + cycle[i:]            
+            new_cost = calculate_cost(new_cycle, matrix_src)
+
+            if new_cost < best_new_cost:
+                best_new_cycle = new_cycle
+                best_new_cost = new_cost
+                chosen_v = closest_v
+
+        cycle = best_new_cycle
+        cost = best_new_cost
+        matrix[:, chosen_v] = np.inf
+
+    return cycle, cost
+
+
+def run_regret2_experiment(path: str):
+    matrix = get_dist_matrix(path)
+
+    solutions = []
+
+    for v in range(200):
+        solutions.append(regret2(matrix, v))
+
+    costs = np.array([cost for sol, cost in solutions])
+    best_sol, best_cost = min(solutions, key=lambda x: x[1])
+    worst_sol, worst_cost = max(solutions, key=lambda x: x[1])
+    avg_cost = np.mean(costs)
+
+    print("Best cost: " + str(best_cost))
+    print("Worst cost: " + str(worst_cost))
+    print("Mean cost after 200 solutions: " + str(avg_cost))
+
+    show_solution(path, best_sol, title="Best Tour")
+    show_solution(path, worst_sol, title="Worst Tour")
--- README.md
+++ README.md
@@ -6,8 +6,12 @@
 |-----------|----------|----------|
 | 148244    | Winiecki | Radosław |
 | 148245    | Szudzik  | Oskar    |
-| 148246    | Kasicka  | Alicja   |
+| 148246    | Kasicka  | Alicja   | (1st assignment)
 
 ## Assignment 1
 
-*TODO*
+Greedy heuristics: https://github.com/Oskshust/evo_comp/tree/main/1
+
+## Assignment 2
+
+Greedy regret heuristics: https://github.com/Oskshust/evo_comp/tree/main/2
